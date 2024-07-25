79b529bdd7fd8a07064b10dc54fdbe1569ca993d
Pratiksha Pimpale <155970383+PratikshaPimpale@users.noreply.github.com>
2024-07-25 06:46:58 +0530

    Add files via upload

---
+++ arithmatic.py
@@ -0,0 +1,9 @@
+a=int(input("Enter number1:"))
+b=int(input("Enter number2:"))
+c=a+b
+print("Addition=",c)
+print("Substraction=",(a-b))
+print("Multiplication=",(a*b))
+print("Division=",(a/b))
+
+      
