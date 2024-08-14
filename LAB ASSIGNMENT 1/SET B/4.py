cd6a3815640f2ec13f230e130b2cd3db373baf3d
Pratiksha Pimpale <155970383+PratikshaPimpale@users.noreply.github.com>
2024-08-14 08:10:26 +0530

    Add files via upload

---
+++ LAB ASSIGNMENT 1/SET B/4.py
@@ -0,0 +1,3 @@
+#4. Write a program which accept an integer value ‘n’ and display all prime numbers till ‘n’. 
+
+
---
+++ LAB ASSIGNMENT 1/SET C/1.py
@@ -0,0 +1,5 @@
+'''1. Write a python script to generate the following pattern upto n lines 
+1 
+1 2 1 
+1 2 3 2 1 
+1 2 3 4 3 2 1'''
